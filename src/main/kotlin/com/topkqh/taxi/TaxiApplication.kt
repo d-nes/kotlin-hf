c4236165d4405913903dfd1886446828e2399d49
Denes Urban <denes.urban@oracle.com>
2023-12-07 09:45:55 +0100

    Adding starter project

---
+++ src/main/kotlin/com/topkqh/taxi/TaxiApplication.kt
@@ -0,0 +1,11 @@
+package com.topkqh.taxi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaxiApplication>(*args)
+}
